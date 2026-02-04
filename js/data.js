6203435b3ddf063c3d242bd8fe8a2e87301c294f
faltas76 <faltas76@gmail.com>
2026-02-04 07:53:24 +0100

    Create data.js

---
+++ js/data.js
@@ -0,0 +1,5 @@
+const drills = [
+  { name:"Aktivace", description:"Rozběhání", duration:10 },
+  { name:"Technika", description:"Práce s míčem", duration:20 },
+  { name:"Hra", description:"Malá hra", duration:30 }
+];
