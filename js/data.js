a97c960158416bffeeb82d533d3440a31e1211bd
faltas76 <faltas76@gmail.com>
2026-02-04 09:00:44 +0100

    Update data.js

--- js/data.js
+++ js/data.js
@@ -1,8 +1,9 @@
-const drills = [
+// js/data.js
+window.drills = [
   {
     id: 1,
     name: "Aktivace s míčem",
-    goal: "technika",
+    goals: ["technika"],
     duration: 10,
     description: "Lehké vedení míče, změny směru.",
     link: "https://www.youtube.com/results?search_query=football+warm+up+with+ball"
@@ -10,26 +11,33 @@ const drills = [
   {
     id: 2,
     name: "Sprinty 10–20 m",
-    goal: "rychlost",
+    goals: ["rychlost"],
     duration: 10,
-    description: "Maximální akcelerace s pauzou.",
+    description: "Maximální akcelerace.",
     link: "https://www.youtube.com/results?search_query=football+sprint+drill"
   },
   {
     id: 3,
-    name: "Silový core",
-    goal: "síla",
+    name: "Core + síla",
+    goals: ["síla"],
     duration: 10,
-    description: "Plank, side plank, dřepy.",
+    description: "Plank, dřepy, stabilita.",
     link: "https://www.youtube.com/results?search_query=football+core+training"
   },
   {
     id: 4,
+    name: "Technika pod tlakem",
+    goals: ["technika"],
+    duration: 15,
+    description: "Přihrávky v pohybu.",
+    link: "https://www.youtube.com/results?search_query=football+passing+drill"
+  },
+  {
+    id: 5,
     name: "Malá hra 5v5",
-    goal: "hra",
+    goals: ["hra"],
     duration: 20,
-    description: "Vysoké tempo, minimum přerušení.",
+    description: "Vysoké tempo.",
     link: "https://www.youtube.com/results?search_query=5v5+small+sided+game+football"
   }
 ];
-
