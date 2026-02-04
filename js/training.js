73af7da048e4b75af6932e109f164e09994b55df
faltas76 <faltas76@gmail.com>
2026-02-04 08:37:56 +0100

    Update training.js

--- js/training.js
+++ js/training.js
@@ -1,9 +1,46 @@
-function render() {
-  document.getElementById("name").innerText = plan[i].name;
-  document.getElementById("desc").innerHTML = `
-    ${plan[i].description}<br>
-    <a href="${plan[i].link}" target="_blank">
-      ▶ Ukázka cvičení
-    </a>
-  `;
+const plan = JSON.parse(localStorage.getItem("trainingPlan")) || [];
+let index = 0;
+let timeLeft = 0;
+let timer;
+
+if (plan.length === 0) {
+  alert("Nebyl nalezen žádný tréninkový plán.");
 }
+
+function startDrill() {
+  const d = plan[index];
+
+  document.getElementById("name").innerText = d.name;
+  document.getElementById("desc").innerHTML = `
+    ${d.description}<br>
+    <a href="${d.link}" target="_blank">▶ Ukázka cvičení</a>
+  `;
+
+  timeLeft = d.duration * 60;
+  clearInterval(timer);
+
+  timer = setInterval(() => {
+    timeLeft--;
+    renderTime();
+    if (timeLeft <= 0) next();
+  }, 1000);
+}
+
+function renderTime() {
+  const min = Math.floor(timeLeft / 60);
+  const sec = timeLeft % 60;
+  document.getElementById("timer").innerText =
+    `${min}:${sec.toString().padStart(2, "0")}`;
+}
+
+function next() {
+  index++;
+  if (index < plan.length) {
+    startDrill();
+  } else {
+    clearInterval(timer);
+    alert("Trénink dokončen 💪");
+  }
+}
+
+startDrill();
