e74af0e0fabf911d9838717910b6c57f1eaf1f89
faltas76 <faltas76@gmail.com>
2026-02-04 08:32:34 +0100

    Update training.js

--- js/training.js
+++ js/training.js
@@ -1,19 +1,9 @@
-let plan = JSON.parse(localStorage.getItem("plan")) || [];
-let i = 0, t = plan[0]?.duration * 60 || 0;
-
 function render() {
   document.getElementById("name").innerText = plan[i].name;
-  document.getElementById("desc").innerText = plan[i].description;
-}
-render();
-
-setInterval(()=>{
-  t--;
-  document.getElementById("timer").innerText =
-    Math.floor(t/60)+":"+String(t%60).padStart(2,"0");
-},1000);
-
-function next(){
-  i++;
-  if(plan[i]){ t=plan[i].duration*60; render(); }
+  document.getElementById("desc").innerHTML = `
+    ${plan[i].description}<br>
+    <a href="${plan[i].link}" target="_blank">
+      ▶ Ukázka cvičení
+    </a>
+  `;
 }
