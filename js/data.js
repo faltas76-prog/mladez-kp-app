37e013962faa894af67bf4ab1173abd2001a28ac
faltas76 <faltas76@gmail.com>
2026-02-04 09:11:31 +0100

    Create data.js

---
+++ js/data.js
@@ -0,0 +1,23 @@
+window.drills = [
+  {
+    name: "Aktivace s míčem",
+    goals: ["technika"],
+    duration: 10,
+    description: "Lehké vedení míče",
+    link: "https://www.youtube.com/results?search_query=football+warm+up+with+ball"
+  },
+  {
+    name: "Sprinty 10–20 m",
+    goals: ["rychlost"],
+    duration: 10,
+    description: "Maximální akcelerace",
+    link: "https://www.youtube.com/results?search_query=football+sprint+drill"
+  },
+  {
+    name: "Malá hra 5v5",
+    goals: ["hra"],
+    duration: 20,
+    description: "Vysoké tempo",
+    link: "https://www.youtube.com/results?search_query=5v5+small+sided+game+football"
+  }
+];
