c45069dad326d8e7f9dee0e1d89007ac29c7e1c5
faltas76 <faltas76@gmail.com>
2026-02-04 08:09:00 +0100

    Update attendance.js

--- js/attendance.js
+++ js/attendance.js
@@ -1,3 +1,22 @@
-function saveAttendance(){
-  alert("Docházka uložena");
+const cat = localStorage.getItem("activeCategory");
+const data = getCategory(cat);
+const list = document.getElementById("list");
+
+data.players.forEach(p => {
+  list.innerHTML += `
+    <div class="card">
+      <label>
+        <input type="checkbox" data-id="${p.id}"> ${p.name}
+      </label>
+    </div>`;
+});
+
+function save() {
+  const records = [];
+  document.querySelectorAll("input").forEach(i => {
+    records.push({ playerId: i.dataset.id, present: i.checked });
+  });
+  data.attendance.push({ date: date.value, records });
+  saveCategory(cat, data);
+  alert("Uloženo");
 }
