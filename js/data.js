69642ce0fdce484c4b421d037d1870c7e2ae1333
faltas76 <faltas76@gmail.com>
2026-02-04 08:32:11 +0100

    Update data.js

--- js/data.js
+++ js/data.js
@@ -1,5 +1,20 @@
 const drills = [
-  { name:"Aktivace", description:"Rozběhání", duration:10 },
-  { name:"Technika", description:"Práce s míčem", duration:20 },
-  { name:"Hra", description:"Malá hra", duration:30 }
+  {
+    name: "Vedení míče slalom",
+    description: "Práce s oběma nohama",
+    duration: 10,
+    link: "https://www.youtube.com/results?search_query=football+dribbling+slalom+drill"
+  },
+  {
+    name: "Sprint na signál",
+    description: "Reakční rychlost",
+    duration: 8,
+    link: "https://www.youtube.com/results?search_query=football+reaction+sprint+drill"
+  },
+  {
+    name: "Hra 5v5",
+    description: "Malá hra vysoké tempo",
+    duration: 20,
+    link: "https://www.youtube.com/results?search_query=5v5+small+sided+game+football"
+  }
 ];
