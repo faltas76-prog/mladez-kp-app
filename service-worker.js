283506081ae9f2945b22d167da4f005a6f37a2c5
faltas76 <faltas76@gmail.com>
2026-02-04 07:56:24 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,24 +1,3 @@
-const CACHE = "kp-mladez-v1";
-const FILES = [
-  "/",
-  "/index.html",
-  "/dashboard.html",
-  "/training.html",
-  "/notes.html",
-  "/players.html",
-  "/attendance.html",
-  "/css/style.css",
-  "/js/app.js"
-];
-
-self.addEventListener("install", e => {
-  e.waitUntil(
-    caches.open(CACHE).then(c => c.addAll(FILES))
-  );
-});
-
-self.addEventListener("fetch", e => {
-  e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request))
-  );
+self.addEventListener("fetch",e=>{
+  e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
 });
