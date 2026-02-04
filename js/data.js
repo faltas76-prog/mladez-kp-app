00be9491daab74e17d97b2f28926dd24a6a7bc4b
faltas76 <faltas76@gmail.com>
2026-02-04 09:33:36 +0100

    Update data.js

--- js/data.js
+++ js/data.js
@@ -1,23 +1,31 @@
-window.drills = [
+const defaultDrills = [
   {
+    id: 1,
     name: "Aktivace s míčem",
     goals: ["technika"],
     duration: 10,
-    description: "Lehké vedení míče",
+    description: "Lehké vedení míče.",
     link: "https://www.youtube.com/results?search_query=football+warm+up+with+ball"
   },
   {
+    id: 2,
     name: "Sprinty 10–20 m",
     goals: ["rychlost"],
     duration: 10,
-    description: "Maximální akcelerace",
+    description: "Maximální akcelerace.",
     link: "https://www.youtube.com/results?search_query=football+sprint+drill"
   },
   {
+    id: 3,
     name: "Malá hra 5v5",
     goals: ["hra"],
     duration: 20,
-    description: "Vysoké tempo",
+    description: "Vysoké tempo.",
     link: "https://www.youtube.com/results?search_query=5v5+small+sided+game+football"
   }
 ];
+
+function getAllDrills() {
+  const custom = JSON.parse(localStorage.getItem("customDrills")) || [];
+  return [...defaultDrills, ...custom];
+}
