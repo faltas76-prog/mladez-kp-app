d657cc38b892b0bc072446224b7ee742e0d37a72
faltas76 <faltas76@gmail.com>
2026-02-05 15:26:45 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,3 +1,40 @@
-self.addEventListener("fetch",e=>{
-  e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
+const CACHE_NAME = "trainer-app-v1";
+
+const FILES = [
+  "./",
+  "./index.html",
+  "./tactical.html",
+  "./players.html",
+  "./notes.html",
+  "./attendance.html",
+  "./manifest.json",
+  "./js/tactical.js",
+  "./js/players.js",
+  "./js/notes.js",
+  "./js/attendance.js"
+];
+
+/* INSTALL */
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES))
+  );
+});
+
+/* ACTIVATE */
+self.addEventListener("activate", e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))
+      )
+    )
+  );
+});
+
+/* FETCH */
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(res => res || fetch(e.request))
+  );
 });
