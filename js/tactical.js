6ae8d125dd4249f0aeed40ad731d1504c231c30d
faltas76 <faltas76@gmail.com>
2026-02-05 16:48:08 +0100

    Update tactical.js

--- js/tactical.js
+++ js/tactical.js
@@ -95,12 +95,9 @@ document.querySelectorAll("[data-tool]").forEach(btn => {
 function pos(e) {
   const rect = canvas.getBoundingClientRect();
 
-  const scaleX = canvas.width / rect.width;
-  const scaleY = canvas.height / rect.height;
-
   return {
-    x: (e.clientX - rect.left) * scaleX,
-    y: (e.clientY - rect.top) * scaleY
+    x: e.clientX - rect.left,
+    y: e.clientY - rect.top
   };
 }
 
