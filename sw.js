805590c124144c9f85b97b18cf4885f00886c9c0
faltas76 <faltas76@gmail.com>
2026-02-12 07:09:59 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,2 +1,39 @@
-"/lineup.html",
-"/js/lineup.js",
+const CACHE_NAME = 'mladez-kp-app-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './tactical.html',
+  './lineup.html',
+  './offline/index.html',
+  './offline/db.js',
+  './offline/sync.js',
+  './js/tactical.js',
+  './js/lineup.js',
+  './manifest.json',
+  './icons/icon-192.png',
+  './icons/icon-512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.map(k => (k !== CACHE_NAME ? caches.delete(k) : null))
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(resp => resp || fetch(event.request))
+  );
+});
